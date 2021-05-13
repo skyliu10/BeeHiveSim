451ba26fa9525371ec36cc2061925d95c80155c2
joebartusek <jfb4@princeton.edu>
2021-05-13 19:19:02 -0400

    fix frame issues tybg

--- src/components/objects/Frame/Frame.js
+++ src/components/objects/Frame/Frame.js
@@ -9,21 +9,22 @@ class Frame extends Group {
         this.name = 'frame';
         let scale = parent.state.scale;
 
-        const backGeometry = new THREE.BoxGeometry(0.02, scale * 500, scale * 1000);
+        const backGeometry = new THREE.BoxGeometry(0.02, scale * 200, scale * 400);
         backGeometry.computeBoundingBox();
 
-        const leftGeometry = new THREE.BoxGeometry(scale * 50, scale * 550, scale * 50);
-        const rightGeometry = new THREE.BoxGeometry(scale * 50, scale * 550, scale * 50);
-        const topGeometry = new THREE.BoxGeometry(scale * 50, scale * 50, scale * 950);
-        const bottomGeometry = new THREE.BoxGeometry(scale * 50, scale * 50, scale * 950);
+        const leftGeometry = new THREE.BoxGeometry(scale * 20, scale * 220, scale * 20);
+        const rightGeometry = new THREE.BoxGeometry(scale * 20, scale * 220, scale * 20);
+        const topGeometry = new THREE.BoxGeometry(scale * 20, scale * 20, scale * 380);
+        const bottomGeometry = new THREE.BoxGeometry(scale * 20, scale * 20, scale * 380);
 
-        leftGeometry.translate(0, 0, scale * 500);
-        rightGeometry.translate(0, 0, scale * -500);
-        topGeometry.translate(0, scale * 250, 0);
-        bottomGeometry.translate(0, scale * -250, 0);
+        leftGeometry.translate(0, 0, scale * 200);
+        rightGeometry.translate(0, 0, scale * -200);
+        topGeometry.translate(0, scale * 100, 0);
+        bottomGeometry.translate(0, scale * -100, 0);
 
         const texture = new THREE.TextureLoader().load('https://threejsfundamentals.org/threejs/lessons/resources/images/compressed-but-large-wood-texture.jpg')
-        const woodMaterial = new THREE.MeshStandardMaterial({ map:texture });
+        const woodMaterial = new THREE.MeshBasicMaterial({ map:texture });
+        //const woodMaterial = new THREE.MeshBasicMaterial({color:0xbf8b52});
 
         const leftMesh = new THREE.Mesh(leftGeometry, woodMaterial);
         const rightMesh = new THREE.Mesh(rightGeometry, woodMaterial);
@@ -37,10 +38,7 @@ class Frame extends Group {
         this.add(bottomMesh);
         this.add(backMesh);
 
-        // Add self to parent's update list
-        parent.addToUpdateList(this);
-
-        this.bb = new THREE.Box3().copy(backGeometry.boundingBox);
+        //this.bb = new THREE.Box3().copy(backGeometry.boundingBox);
         //var bbHelper = new THREE.Box3Helper(this.bb, 0xffff00);
         //this.add(bbHelper);
 
